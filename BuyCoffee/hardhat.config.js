ef97fdba599313c0d339b7a7b0f9d474888e1d79
morsoli <15901212835@163.com>
2022-11-18 01:09:18 +0800

    onchain nft Dapp

--- BuyCoffee/hardhat.config.js
+++ BuyCoffee/hardhat.config.js
@@ -1,16 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_URL = process.env.ALCHEMY_APP_ID;
-const PRIVATE_KEY = process.env.ACCOUNT;
+const GOERLI_URL = process.env.ALCHEMY_GOERLI_URL;
+const MUMBAI_URL = process.env.ALCHEMY_MUMBAI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
   networks: {
     goerli: {
       url: GOERLI_URL,
       accounts: [PRIVATE_KEY]
-    }
+    },
+    mumbai: {
+      url: MUMBAI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+    },   
   }
 };
